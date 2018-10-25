bde8f195190d372c64eaf318c71d3bbecefaa84a
foureyez <abhirawat1304@gmail.com>
2018-10-25 16:04:46 +0530

    Changing Project structure to make the library publishable

--- projects/foureyez/ng-data-table/src/public_api.ts
+++ projects/foureyez/ng-data-table/src/public_api.ts
@@ -1,8 +1,7 @@
 /*
- * Public API Surface of data-table
+ * Public API Surface of ng-data-table
  */
 
 export * from './lib/data-table.service';
 export * from './lib/data-table.component';
 export * from './lib/data-table.module';
-export * from './lib/model/column';
